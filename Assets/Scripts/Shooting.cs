104fefc9bc1619ea51968222d7083db3f67a91f3
Pluhec <jakplu@seznam.cz>
2025-02-11 20:20:04 +0100

    Special ability - funguje tak 50/50

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,12 +7,20 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform nozzle;
     public float bulletSpeed = 20f;
+    public float specialAbilitySpeed = 40f;
+    public int specialAbilityCount = 3;
+    public float specialAbilityCooldown = 10f;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ShootSpecialAbility();
         }
     }
 
@@ -21,5 +29,22 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, nozzle.position, Quaternion.identity);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.linearVelocity = Vector2.up * bulletSpeed;
+    }
+
+    void ShootSpecialAbility()
+    {
+        float angleStep = 120f / (specialAbilityCount - 1);
+        float startAngle = -60f;
+
+        for (int i = 0; i < specialAbilityCount; i++)
+        {
+            float angle = startAngle + i * angleStep;
+            float angleRad = angle * Mathf.Deg2Rad;
+            Vector2 direction = new Vector2(Mathf.Sin(angleRad), Mathf.Cos(angleRad));
+
+            GameObject bullet = Instantiate(bulletPrefab, nozzle.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = direction * specialAbilitySpeed;
+        }
     }
 }
--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class bullet : MonoBehaviour
 {
+    public float bulletDamage = 1;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "bulletDeleteBarrier")
